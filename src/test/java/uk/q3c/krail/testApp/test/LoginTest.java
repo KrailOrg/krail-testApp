c166c8f92adfeb77a82d014f515d602d73a788b5
David Sowerby <david.sowerby@virgin.net>
2015-02-12 20:01:40 +0000

    fix davidsowerby/krail#322 Rectified failure after login
    The UserSitemap correctly does not contain the login node in its map after login, but the NavigationRule was attempting to use it.  Provided some utility methods in UserSitemap to assist, isLoginUri(), isLogoutUri etc, and also modified the DefaultNavigator.currentNode() method to check specifically for the login node.
    
    Also added some debug statements
    Added equals() methods to Master and UserSitemapNode
    Made DefaultMasterSitemap threadsafe (see #244)

--- src/test/java/uk/q3c/krail/testApp/test/LoginTest.java
+++ src/test/java/uk/q3c/krail/testApp/test/LoginTest.java
@@ -1,14 +1,12 @@
 /*
- * Copyright (c) 2014 David Sowerby
+ * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
- * the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.krail.testApp.test;
@@ -67,22 +65,7 @@ public class LoginTest extends KrailTestBenchTestCase {
         assertThat(loginStatus.username()).isEqualTo("ds");
     }
 
-    @Test
-    public void loginFromLogout() {
 
-        // given
-        login();
-        pause(500);
-        loginStatus.loginButton()
-                   .click();
-        pause(500);
-        verifyUrl("logout");
-        // when
-        login();
-        // then
-        pause(500);
-        verifyUrl("private/home");
-    }
 
     /**
      * If previous tests have caused unsuccessful logins, or this test is run repeatedly, login will fail because of
---
+++ src/test/java/uk/q3c/krail/testApp/test/Login_Navigation_Rule.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2015. David Sowerby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package uk.q3c.krail.testApp.test;
+
+import com.vaadin.testbench.ScreenshotOnFailureRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import uk.q3c.krail.testbench.KrailTestBenchTestCase;
+import uk.q3c.krail.testbench.page.object.NavTreePageObject;
+
+public class Login_Navigation_Rule extends KrailTestBenchTestCase {
+
+    @Rule
+    public ScreenshotOnFailureRule screenshotOnFailureRule = new ScreenshotOnFailureRule(this, true);
+    private NavTreePageObject navTree = new NavTreePageObject(this);
+
+    @Before
+    public void setUp() throws Exception {
+        appContext = "krail-testapp";
+    }
+
+    @Test
+    public void straight_to_login() {
+        //given
+        navigateTo("login");
+        //when
+        login();
+        //then
+        verifyUrl("private/home");
+    }
+
+    @Test
+    public void public_page_then_login() {
+        //given
+        navigateTo("system-account/refresh-account");
+        //when
+        login();
+        //then
+        verifyUrl("system-account/refresh-account");
+    }
+
+    @Test
+    public void login_out_in() {
+        //given
+        navigateTo("system-account/refresh-account");
+        login();
+        loginStatus.loginButton()
+                   .click(); // logout
+        //when
+        login();
+        pause(500);
+        //then
+
+        verifyUrl("private/home");
+    }
+
+    @Test
+    public void straight_to_logout_then_login() {
+        //given
+        navigateTo("logout");
+        //when
+        login();
+        pause(500);
+        //then
+        verifyUrl("private/home");
+    }
+
+
+    public void init(WebDriver driver, String baseUrl) {
+        this.setDriver(driver);
+        this.setBaseUrl(baseUrl);
+    }
+
+}
