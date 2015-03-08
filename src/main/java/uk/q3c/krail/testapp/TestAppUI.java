9e926d562768297c15bce64c47d04595271112f3
David Sowerby <david.sowerby@virgin.net>
2015-03-08 22:26:09 +0000

    See davidsowerby/krail#345 Locale change listeners replaced by event bus

--- src/main/java/uk/q3c/krail/testapp/TestAppUI.java
+++ src/main/java/uk/q3c/krail/testapp/TestAppUI.java
@@ -1,14 +1,12 @@
 /*
- * Copyright (c) 2014 David Sowerby
+ * Copyright (c) 2015. David Sowerby
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
- * the specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 
 package uk.q3c.krail.testapp;
@@ -18,6 +16,9 @@ import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.converter.ConverterFactory;
 import com.vaadin.server.ErrorHandler;
+import net.engio.mbassy.bus.MBassador;
+import uk.q3c.krail.core.eventbus.BusMessage;
+import uk.q3c.krail.core.eventbus.SessionBus;
 import uk.q3c.krail.core.navigate.Navigator;
 import uk.q3c.krail.core.push.Broadcaster;
 import uk.q3c.krail.core.push.PushMessageRouter;
@@ -45,11 +46,11 @@ public class TestAppUI extends DefaultApplicationUI {
                         UserNavigationMenu menu, UserNavigationTree navTree, Breadcrumb breadcrumb,
                         SubPagePanel subpage, MessageBar messageBar, Broadcaster broadcaster,
                         PushMessageRouter pushMessageRouter, SessionObject sessionObject,
-                        ApplicationTitle applicationTitle, Translate translate, CurrentLocale currentLocale,
-                        I18NProcessor translator, LocaleSelector localeSelector, UserNotifier userNotifier) {
+                        ApplicationTitle applicationTitle, Translate translate, CurrentLocale currentLocale, I18NProcessor translator, LocaleSelector
+                                localeSelector, UserNotifier userNotifier, @SessionBus MBassador<BusMessage> eventBus) {
         super(navigator, errorHandler, converterFactory, logo, header, userStatus, menu, navTree, breadcrumb,
-                subpage, messageBar, broadcaster, pushMessageRouter, applicationTitle, translate, currentLocale,
-                translator, localeSelector, userNotifier);
+                subpage, messageBar, broadcaster, pushMessageRouter, applicationTitle, translate, currentLocale, translator, localeSelector, userNotifier,
+                eventBus);
 
     }
 
