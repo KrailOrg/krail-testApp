c240ed632627d4f7c218bc1999a27a0026aa2d29
David Sowerby <david.sowerby@virgin.net>
2015-05-19 11:05:33 +0100

    Vaadin 7.4.6

--- src/test/java/uk/q3c/krail/testApp/test/VaadinVersionTest.java
+++ src/test/java/uk/q3c/krail/testApp/test/VaadinVersionTest.java
@@ -33,6 +33,6 @@ public class VaadinVersionTest extends KrailTestBenchTestCase {
         //then
         assertThat(VersionUtil.getVaadinMajorVersion(getDriver())).isEqualTo(7);
         assertThat(VersionUtil.getVaadinMinorVersion(getDriver())).isEqualTo(4);
-        assertThat(VersionUtil.getVaadinRevision(getDriver())).isEqualTo(5);
+        assertThat(VersionUtil.getVaadinRevision(getDriver())).isEqualTo(6);
     }
 }
