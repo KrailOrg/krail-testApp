fe00d719b9fd27e7b7ebff50bb992b47faa3aa3f
David Sowerby <david.sowerby@virgin.net>
2018-04-15 09:35:33 +0100

    Using krail 0.16.0.0 from serial_1 branch, all tests pass except for serialisation tests.
    Serialisation tests failing becuase of a test setup issue
    
    Dependency substitution for commons-beanutils required in a Krail application (and therefore this test application), even though it is substituted in the core Krail library.  Leave this as a workaround until BVal updated / replaced

--- src/test/java/uk/q3c/krail/TestServiceSerial.java
+++ src/test/java/uk/q3c/krail/TestServiceSerial.java
@@ -4,7 +4,6 @@ import com.google.inject.Inject;
 import uk.q3c.krail.eventbus.MessageBus;
 import uk.q3c.krail.i18n.Translate;
 import uk.q3c.krail.service.AbstractService;
-import uk.q3c.krail.service.RelatedServiceExecutor;
 import uk.q3c.krail.service.ServiceKey;
 import uk.q3c.krail.testapp.i18n.LabelKey;
 import uk.q3c.util.guice.SerializationSupport;
@@ -18,8 +17,8 @@ import java.io.ObjectOutputStream;
 public class TestServiceSerial extends AbstractService {
 
     @Inject
-    protected TestServiceSerial(Translate translate, MessageBus messageBus, RelatedServiceExecutor servicesExecutor, SerializationSupport serializationSupport) {
-        super(translate, messageBus, servicesExecutor, serializationSupport);
+    protected TestServiceSerial(Translate translate, MessageBus messageBus, SerializationSupport serializationSupport) {
+        super(translate, messageBus, serializationSupport);
     }
 
     @Override
